a8a5c078cec57a044c9a022999455f077df68240
hikaruna <me@hikaruna.net>
2020-04-26 16:46:47 +0000

    add node-server host port custom feature

--- node-server-by-grpc_tools_node_protoc_ts/main.ts
+++ node-server-by-grpc_tools_node_protoc_ts/main.ts
@@ -1,10 +1,20 @@
 import { Server, ServerCredentials } from 'grpc'
 import { ChatService } from './lib/chat-service/chat_grpc_pb';
 import { ChatServer } from './src/ChatServer';
+import { program } from 'commander';
+
+program
+  .requiredOption('-h, --host <host>', 'bind host, default to 0.0.0.0', '0.0.0.0')
+  .requiredOption('-p, --port <port>', 'bind port, default 9090', '9090')
+
+program.parse(process.argv);
 
 const server = new Server();
 server.addService(ChatService, new ChatServer());
-if (server.bind('0.0.0.0:9090', ServerCredentials.createInsecure()) === 0) {
+
+const address = `${program.host}:${program.port}`
+if (server.bind(address, ServerCredentials.createInsecure()) === 0) {
   throw new Error('bind error');
 }
 server.start();
+console.log(`server started on ${address}`)
